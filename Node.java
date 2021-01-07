a8cae8308dd928bae66b72f4e5e23af44a90d9c1
vli21 <vli21@stuy.edu>
2021-01-07 18:11:11 -0500

    able to set node data and retreieve node data

--- Node.java
+++ Node.java
@@ -1,6 +1,14 @@
 public class Node{
- public Node(String value){/*create a constructor*/}
  private String data;
  private Node next,prev;
+
+ public Node(String value){
+   data=value;
+ }
+
+ public String getData(){
+   return data;
+ }
+
  //write get/set methods for all three instance variables.
 }
