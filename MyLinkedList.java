3b0e6f4427cc4aace9f719dc1199f80b9e9e8a00
vli21 <vli21@stuy.edu>
2021-01-07 20:50:33 -0500

    size and add created, may need further debugging

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,12 +1,32 @@
 public class MyLinkedList{
  private int size;
  private Node start,end;
- public MyLinkedList(){/*create a constructor*/}
- public int size();
- public boolean add(String value);
- public boolean add(int index, String value);
- public String get(int index);
- public String set(int index, String value);
- public String toString();
+ public MyLinkedList(){
+   start=null;
+   end =null;
+ }
+ public int size(){
+   return size;
+ }
+ public boolean add(String value){
+   Node NewNode= new Node(value);
+   if (start== null){
+     start = end = NewNode;
+     size++;
+   }
+   return true;
+ }
+ public boolean add(int index, String value){
+   return true;
+ }
+ public String get(int index){
+   return "";
+ }
+ public String set(int index, String value){
+   return "";
+ }
+ public String toString(){
+   return "";
+ }
  //Any helper method that returns a Node object MUST BE PRIVATE!
 }
