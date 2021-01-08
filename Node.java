81c09293071b180cc2ab2b75c4cedeae3b4a7e70
vli21 <vli21@stuy.edu>
2021-01-07 20:47:08 -0500

    set prev and next

--- Node.java
+++ Node.java
@@ -4,10 +4,20 @@ public class Node{
 
  public Node(String value){
    data=value;
+   next=null;
+   prev=null;
  }
 
  public String getData(){
    return data;
+ }
+
+ public Node Next(){
+   return next;
+ }
+
+ public Node Prev(){
+   return prev;
  }
 
  //write get/set methods for all three instance variables.
